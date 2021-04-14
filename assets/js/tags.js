d99b43876f345f9f3574d2f626f20171303eb9b1
David SALLE <freezed.or.frozen@gmail.com>
2021-04-14 09:05:09 +0200

    Rename custome.js to upload.js and add tags.js

---
+++ assets/js/tags.js
@@ -0,0 +1,9 @@
+/**
+ * YASLOB : Yet Another Simple Library Of eBooks
+ * Handle tags in Javascript for the Views (V of MVC)
+ * 
+ * @author David SALLÉ
+ * @date April 2021
+ * @license MIT
+ */
+
--- templates/footer.php
+++ templates/footer.php
@@ -15,6 +15,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.7.570/pdf.min.js"></script>
-    <script src="./assets/js/custom.js"></script>
+    <script src="./assets/js/upload.js"></script>
+    <script src="./assets/js/tags.js"></script>
 </body>
 </html>
